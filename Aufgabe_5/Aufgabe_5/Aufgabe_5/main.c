582f511092eb9ef8056e44ac9a6e3c9889dc2bd2
André <45215556+andre-albers@users.noreply.github.com>
2019-05-22 15:19:44 +0200

    CR

---
+++ Aufgabe_5/Aufgabe_5/Aufgabe_5/main.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdlib.h>
+#include <stdio.h>
+
+int addsub(int op1, int op2, char what, unsigned int* flags)
+{
+
+
+
+}
+
+int fehlermeldungen(int fehlerid)
+{
+
+
+
+
+}
+
+int main(int argc, char **argv[])
+{
+
+
+}
+
+
+
+
+
+
